437cfd179747276a30b4530610e7743fa5aa95ce
Dsoungpa <dsoungpa@ucsc.edu>
2023-07-24 17:53:54 -0700

    Added Fade transition

--- slideshow.js
+++ slideshow.js
@@ -1,31 +1,28 @@
-// This addEventListener makes sure that the HTML is fully loaded
-// before running the .js file
 document.addEventListener("DOMContentLoaded", function () {
-    // JavaScript for image cycle
     let slideIndex = 0;
     showSlides();
-
+  
     function showSlides() {
-        // Get all slide elements
-        const slides = document.getElementsByClassName("mySlides");
-
-        // Hide all slides
-        for (let i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-
-        // Show the current slide
-        slides[slideIndex].style.display = "block";
-
-        // Increment the slide index
-        slideIndex++;
-
-        // Reset the index if it goes beyond the number of slides
-        if (slideIndex >= slides.length) {
-            slideIndex = 0;
-        }
-
-        // Repeat the slideshow every 4 seconds (adjust the duration as needed)
-        setTimeout(showSlides, 4000);
+      const slides = document.getElementsByClassName("mySlides");
+  
+      // Hide all slides by setting opacity to 0
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = 0;
+      }
+  
+      // Reset the index if it goes beyond the number of slides
+      if (slideIndex >= slides.length) {
+        slideIndex = 0;
+      }
+  
+      // Show the current slide with a fade-in effect by changing opacity to 1
+      slides[slideIndex].style.opacity = 1;
+  
+      // Increment the slide index
+      slideIndex++;
+  
+      // Repeat the slideshow every 4 seconds (adjust the duration as needed)
+      setTimeout(showSlides, 4000);
     }
-});
+  });
+  
